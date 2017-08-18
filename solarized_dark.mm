3af23307712544580af6e19d4ed761f19acc4d1a
Pjurek3 <Pjurek3@gmail.com>
2017-08-17 20:32:54 -0700

    added template mm files

---
+++ solarized_dark.mm
@@ -0,0 +1,105 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_TEXT="new_mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_619713213" CREATED="1500518499984" MODIFIED="1500953294585"><hook NAME="MapStyle" background="#002b36">
+    <properties fit_to_viewport="false;" edgeColorConfiguration="#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff,#657b83ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<edge COLOR="#657b83"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="bubble">
+<edge COLOR="#657b83"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#839496" BACKGROUND_COLOR="#073642">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#839496" BACKGROUND_COLOR="#073642">
+<font SIZE="9"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#839496" BACKGROUND_COLOR="#073642" TEXT_ALIGN="LEFT">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#839496" BACKGROUND_COLOR="#073642">
+<edge STYLE="hide_edge" COLOR="#657b83"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="bubble">
+<edge COLOR="#657b83"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#839496" BACKGROUND_COLOR="#073642">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#93a1a1" BACKGROUND_COLOR="#073642">
+<icon BUILTIN="yes"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode TEXT="task_1" COLOR="#93a1a1" BACKGROUND_COLOR="#073642">
+<icon BUILTIN="full-1"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#839496" BACKGROUND_COLOR="#073642" STYLE="bubble">
+<edge COLOR="#657b83"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#93a1a1" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#839496" BACKGROUND_COLOR="#002b36" STYLE="bubble">
+<font SIZE="16"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<font SIZE="14"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<font SIZE="12"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<font SIZE="10"/>
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#839496" BACKGROUND_COLOR="#002b36">
+<edge COLOR="#657b83"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+</node>
+</map>
---
+++ solarized_light.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_TEXT="new_mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_619713213" CREATED="1500518499984" MODIFIED="1501349481726"><hook NAME="MapStyle" background="#fdf6e3">
+    <properties fit_to_viewport="false;" edgeColorConfiguration="#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff,#839496ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<edge COLOR="#839496"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="bubble">
+<edge COLOR="#839496"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5">
+<font SIZE="9"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" TEXT_ALIGN="LEFT">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5">
+<edge STYLE="hide_edge" COLOR="#839496"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="bubble">
+<edge COLOR="#839496"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#0087ff" BACKGROUND_COLOR="#eee8d5">
+<icon BUILTIN="yes"/>
+<edge COLOR="#839496"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#657b83" BACKGROUND_COLOR="#eee8d5" STYLE="bubble">
+<edge COLOR="#839496"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble">
+<font SIZE="16"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="14"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="12"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="10"/>
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#657b83" BACKGROUND_COLOR="#fdf6e3">
+<edge COLOR="#839496"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+</node>
+</map>
